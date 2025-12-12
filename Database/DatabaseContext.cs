8effc2884919005a5e341fe901535f19064e208d
sahit <sahit@HONEY>
2025-12-12 12:42:57 +0530

    Add project files.

---
+++ Database/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿namespace UserRoleMangement.Database
+{
+    using Microsoft.EntityFrameworkCore;
+    using UserRoleMangement.Models;
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+    }
+}
---
+++ Models/Role.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserRoleMangement.Models
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public string? RoleName { get; set; }
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
