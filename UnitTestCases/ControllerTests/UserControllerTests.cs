ccdfd9d8a9ffaaee7b14ee5f812391a07a277e74
Praveenkumar7382 <Praveen.kumarbodewar@gmail.com>
2025-12-14 13:23:53 +0530

    Added UnitTestCases project

---
+++ UnitTestCases/ControllerTests/UserControllerTests.cs
@@ -0,0 +1,200 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using UserRoleMangement.Controllers;
+using UserRoleMangement.Database.Repositories.Interfaces;
+using UserRoleMangement.Models;
+
+namespace UnitTestCases.ControllerTests
+{
+    public class UserControllerTests
+    {
+        private UserController _controller = null!;
+        private Mock<IUserRepository> _userRepoMock = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            _userRepoMock = new Mock<IUserRepository>();
+            _controller = new UserController(_userRepoMock.Object);
+        }
+
+        [Test]
+        public async Task GetAll_WhenCalled_ReturnsOk()
+        {
+            List<User> users =
+            [
+            new User { UserId = 1, UserName = "Admin" },
+            new User { UserId = 2, UserName = "User" }
+            ];
+
+            _userRepoMock
+                .Setup(r => r.GetAllAsync())
+                .ReturnsAsync(users);
+
+            var result = await _controller.Get();
+
+            var okResult = result as OkObjectResult;
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(okResult, Is.Not.Null);
+                Assert.That(users, Is.EqualTo(okResult!.Value));
+            });
+        }
+
+        [Test]
+        public async Task GetById_WhenUserExists_ReturnsOk()
+        {
+            var user = new User { UserId = 1, UserName = "Admin" };
+
+            _userRepoMock
+                .Setup(r => r.GetById(1))
+                .ReturnsAsync(user);
+
+            var result = await _controller.Get(1);
+
+            var okResult = result as OkObjectResult;
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(okResult, Is.Not.Null);
+                Assert.That(user, Is.EqualTo(okResult!.Value));
+            });
+        }
+
+        [Test]
+        public async Task GetById_WhenUserDoesNotExist_ReturnsNotFound()
+        {
+            _userRepoMock
+                .Setup(r => r.GetById(1))!
+                .ReturnsAsync((User?)null);
+
+            var result = await _controller.Get(1);
+
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        [Test]
+        public async Task Create_WhenUserCreationFails_ReturnsBadRequest()
+        {
+            var user = new User { UserName = "Test" };
+
+            _userRepoMock
+                .Setup(r => r.AddAsync(user))
+                .ReturnsAsync((false, "User exists", null));
+
+            var result = await _controller.Create(user);
+
+            var badRequest = result as BadRequestObjectResult;
+
+            Assert.NotNull(badRequest);
+        }
+
+        [Test]
+        public async Task Create_WhenUserCreatedSuccessfully_ReturnsOk()
+        {
+            var user = new User
+            {
+                UserId = 1,
+                UserName = "Admin",
+                Email = "admin@test.com",
+                Role = new Role { RoleName = "Admin", Description = "Admin role" }
+            };
+
+            _userRepoMock
+                .Setup(r => r.AddAsync(It.IsAny<User>()))
+                .ReturnsAsync((true, "User added successfully", user));
+
+            var result = await _controller.Create(user);
+
+            var okResult = result as OkObjectResult;
+            Assert.NotNull(okResult);
+        }
+
+        [Test]
+        public async Task ForgotPassword_WhenUserNotFound_ReturnsBadRequest()
+        {
+            var request = new ForgotPassword { UserName = "test" };
+
+            _userRepoMock
+                .Setup(r => r.ForgotPasswordAsync(request))
+                .ReturnsAsync((false, "User Name does not exist"));
+
+            var result = await _controller.ForgotPassword(request);
+
+            Assert.IsInstanceOf<BadRequestObjectResult>(result);
+        }
+
+        [Test]
+        public async Task ForgotPassword_WhenSuccess_ReturnsOk()
+        {
+            var request = new ForgotPassword { UserName = "test" };
+
+            _userRepoMock
+                .Setup(r => r.ForgotPasswordAsync(request))
+                .ReturnsAsync((true, "Password updated"));
+
+            var result = await _controller.ForgotPassword(request);
+
+            Assert.IsInstanceOf<OkObjectResult>(result);
+        }
+
+        [Test]
+        public async Task Update_WhenUserDoesNotExist_ReturnsNotFound()
+        {
+            var user = new User { UserName = "Test" };
+
+            _userRepoMock
+                .Setup(r => r.UpdateAsync(It.IsAny<User>()))!
+                .ReturnsAsync((User?)null);
+
+            var result = await _controller.Update(1, user);
+
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        [Test]
+        public async Task Update_WhenUserExists_ReturnsOk()
+        {
+            var user = new User
+            {
+                UserId = 1,
+                UserName = "Updated",
+                Email = "updated@test.com"
+            };
+
+            _userRepoMock
+                .Setup(r => r.UpdateAsync(It.IsAny<User>()))
+                .ReturnsAsync(user);
+
+            var result = await _controller.Update(1, user);
+
+            Assert.IsInstanceOf<OkObjectResult>(result);
+        }
+
+        [Test]
+        public async Task Delete_WhenUserDoesNotExist_ReturnsNotFound()
+        {
+            _userRepoMock
+                .Setup(r => r.DeleteAsync(1))
+                .ReturnsAsync(false);
+
+            var result = await _controller.Delete(1);
+
+            Assert.IsInstanceOf<NotFoundResult>(result);
+        }
+
+        [Test]
+        public async Task Delete_WhenUserExists_ReturnsOk()
+        {
+            _userRepoMock
+                .Setup(r => r.DeleteAsync(1))
+                .ReturnsAsync(true);
+
+            var result = await _controller.Delete(1);
+
+            Assert.IsInstanceOf<OkObjectResult>(result);
+        }
+    }
+}
---
+++ UnitTestCases/Repositories/RoleRepositoryTests.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using UserRoleMangement.Database;
+using UserRoleMangement.Database.Repositories;
+using UserRoleMangement.Models;
+
+namespace UnitTestCases.Repositories
+{
+    public class RoleRepositoryTests
+    {
+        private DatabaseContext _context = null!;
+        private RoleRepository _repository = null!;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase("RoleDb")
+                .Options;
+
+            _context = new DatabaseContext(options);
+            _repository = new RoleRepository(_context);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
+        }
+
+        [Test]
+        public async Task GetAllAsync_WhenCalled_ReturnsAllRoles()
+        {
+            _context.Roles.AddRange(
+                new Role { RoleName = "Admin" },
+                new Role { RoleName = "User" }
+            );
+            await _context.SaveChangesAsync();
+
+            var result = await _repository.GetAllAsync();
+
+            Assert.That(result.Count(), Is.EqualTo(2));
+        }
+
+        [Test]
+        public async Task GetById_WhenRoleExists_ReturnsRole()
+        {
+            var role = new Role { RoleName = "Admin" };
+            _context.Roles.Add(role);
+            await _context.SaveChangesAsync();
+
+            var result = await _repository.GetById(role.RoleId);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(role.RoleName, Is.EqualTo(result!.RoleName));
+        }
+
+        [Test]
+        public async Task GetByName_WhenRoleExists_ReturnsTrueAndRoleId()
+        {
+            var role = new Role { RoleName = "Admin" };
+            _context.Roles.Add(role);
+            await _context.SaveChangesAsync();
+
+            var result = await _repository.GetByName("Admin");
+
+            Assert.That(result.IsExixtingRole, Is.True); ;
+            Assert.That(result.Id, Is.EqualTo(role.RoleId));
+        }
+
+        [Test]
+        public async Task GetByName_WhenRoleDoesNotExist_ReturnsFalseAndZero()
+        {
+            var result = await _repository.GetByName("Unknown");
+
+            Assert.That(result.IsExixtingRole, Is.False);
+            Assert.That(result.Id, Is.EqualTo(0));
+        }
+
+        [Test]
+        public async Task AddAsync_WhenRoleDoesNotExist_AddsNewRole()
+        {
+            var role = new Role { RoleName = "Admin", Description = "Admin role" };
+
+            var result = await _repository.AddAsync(role);
+
+            var rolesInDb = await _context.Roles.ToListAsync();
+
+            Assert.That(rolesInDb.Count, Is.EqualTo(1));
+            Assert.That(rolesInDb[0].RoleName, Is.EqualTo(role.RoleName));
+        }
+
+        [Test]
+        public async Task AddAsync_WhenRoleAlreadyExists_DoesNotCreateDuplicate()
+        {
+            var existingRole = new Role { RoleName = "Admin" };
+            _context.Roles.Add(existingRole);
+            await _context.SaveChangesAsync();
+
+            var role = new Role { RoleName = "Admin" };
+
+            var result = await _repository.AddAsync(role);
+
+            var rolesInDb = await _context.Roles.ToListAsync();
+
+            Assert.That(rolesInDb.Count, Is.EqualTo(1));
+            Assert.That(result.RoleId, Is.EqualTo(existingRole.RoleId));
+        }
+    }
+}
