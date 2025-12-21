703ccd94b2e3849fe2dac32d7df38a8e9dfa6ed9
Praveenkumar7382 <Praveen.kumarbodewar@gmail.com>
2025-12-21 11:37:43 +0530

    Merge initialsetup into master

--- UnitTestCases/obj/Debug/net10.0/UnitTestCases.AssemblyInfo.cs
+++ UnitTestCases/obj/Debug/net10.0/UnitTestCases.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTestCases")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccdfd9d8a9ffaaee7b14ee5f812391a07a277e74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2062853cceae7d2b8d8dabc63270edc5a7a5661")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTestCases")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTestCases")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
